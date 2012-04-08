6a85521cfdaa25508c87f0b5f35bae432d2a82ac
avijeet <avijeet.singh@hotmail.com>
2012-04-08 11:06:26 +0530

    code check in

---
+++ PhoneApp1/PhoneApp1/Model/EchoNestArtistTopHot.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+
+//http://developer.echonest.com/api/v4/artist/top_hottt?api_key=N6E4NIOVYMTHNDM8J&format=json&results=20&start=0&bucket=hotttnesss
+namespace PhoneApp1.Model
+{
+    public class Status
+    {
+        public string version { get; set; }
+        public int code { get; set; }
+        public string message { get; set; }
+    }
+
+    public class Artist
+    {
+        public string name { get; set; }
+        public double hotttnesss { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Response
+    {
+        public Status status { get; set; }
+        public List<Artist> artists { get; set; }
+    }
+
+    public class EchoNestArtistTopHot
+    {
+        public Response response { get; set; }
+    }
+}
---
+++ PhoneApp1/PhoneApp1/Model/EightTrackDataService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using PhoneApp1.DataServices;
+
+namespace PhoneApp1.Model
+{
+    public class EightTrackDataService
+    {
+        public Uri eighttracksmix = new Uri("http://8tracks.com/mixes.json?api_key=5aca54a2ba7c7a3ef7c5d7410738021bdcf742ec"); 
+
+        public void getEightTrackMixes()
+        {
+            WebClient web = new WebClient();
+            web.DownloadStringCompleted += new DownloadStringCompletedEventHandler(getEightTrackMixes_Completed);
+            web.DownloadStringAsync(eighttracksmix);
+        }
+
+        void getEightTrackMixes_Completed(object sender, DownloadStringCompletedEventArgs e)
+        {
+            byte[] encodedString = Encoding.UTF8.GetBytes(e.Result);
+
+            //// Put the byte array into a stream and rewind it to the beginning
+            MemoryStream ms = new MemoryStream(encodedString);
+            ms.Flush();
+            ms.Position = 0;
+
+            // convert json result to model
+            Stream stream = ms;
+            DataContractJsonSerializer dataContractJsonSerializer = new DataContractJsonSerializer(typeof(EightTracksMixes));
+            EightTracksMixes myResults =
+            (EightTracksMixes)dataContractJsonSerializer.ReadObject(stream);
+
+            var result = myResults;
+
+        }
+    }
+}
