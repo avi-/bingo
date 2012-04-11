61df936dd13ed0f9f9889d8c4fbc80b36d7bb6bc
avijeet <avijeet.singh@hotmail.com>
2012-04-12 01:09:29 +0530

    checkin for the poc

--- code/PhoneApp1/PhoneApp1/MainPage.xaml.cs
+++ code/PhoneApp1/PhoneApp1/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using PhoneApp1.Model;
+using Microsoft.Phone.BackgroundAudio;
 
 namespace PhoneApp1
 {
@@ -26,7 +27,15 @@ namespace PhoneApp1
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             EightTrackDataService obj = new EightTrackDataService();
-            obj.getEightTrackPlayToken();
+            obj.playEightTrackSong("807789426", "31453", (se, ev) =>
+                {
+                    MyAudioPlaybackAgent.AudioPlayer player = new MyAudioPlaybackAgent.AudioPlayer();
+
+                    player.getTrack(se.set.track.name, se.set.track.performer, se.set.track.release_name, se.set.track.url);
+                    BackgroundAudioPlayer.Instance.Play();
+                });
+            
+           
         }
     }
 }
--- code/PhoneApp1/PhoneApp1/Model/EightTrackDataService.cs
+++ code/PhoneApp1/PhoneApp1/Model/EightTrackDataService.cs
@@ -17,13 +17,19 @@ namespace PhoneApp1.Model
 {
     public class EightTrackDataService
     {
+        Action<EightTracksMixes, Exception> GetEightTrackMixesCallback;
+        Action<EightTrackPlayToken, Exception> GetEightTrackPlayTokenCallback;
+        Action<EightTrackPlaySong, Exception> PlayEightTrackPlaySongCallback;
+
         public Uri eighttracksmix = new Uri("http://8tracks.com/mixes.json?api_key=5aca54a2ba7c7a3ef7c5d7410738021bdcf742ec");
         public Uri eightrackplaytoken = new Uri("http://8tracks.com/sets/new.json?api_key=5aca54a2ba7c7a3ef7c5d7410738021bdcf742ec");
 
-        public void getEightTrackMixes()
+        public void getEightTrackMixes(Action<EightTracksMixes,Exception>callback)
         {
             WebClient web = new WebClient();
             web.DownloadStringCompleted += new DownloadStringCompletedEventHandler(getEightTrackMixes_Completed);
+
+            GetEightTrackMixesCallback = callback;
             web.DownloadStringAsync(eighttracksmix);
         }
 
@@ -43,13 +49,15 @@ namespace PhoneApp1.Model
             (EightTracksMixes)dataContractJsonSerializer.ReadObject(stream);
 
             var result = myResults;
+            GetEightTrackMixesCallback(myResults, null);
 
         }
 
-        public void getEightTrackPlayToken()
+        public void getEightTrackPlayToken(Action<EightTrackPlayToken,Exception>callback)
         {
             WebClient web = new WebClient();
             web.DownloadStringCompleted += new DownloadStringCompletedEventHandler(getEightTrackPlayToken_Completed);
+            GetEightTrackPlayTokenCallback = callback;
             web.DownloadStringAsync(eightrackplaytoken);
 
         }
@@ -70,15 +78,18 @@ namespace PhoneApp1.Model
             (EightTrackPlayToken)dataContractJsonSerializer.ReadObject(stream);
 
             var result = myResults;
+            GetEightTrackPlayTokenCallback(myResults, null);
 
         }
 
-        public void playEightTrackSong(string playtoken, string songID)
+        public void playEightTrackSong(string playtoken, string songID,Action<EightTrackPlaySong,Exception>callback)
         {
             string uri = "http://8tracks.com/sets/" + playtoken + "/play.json?api_key=5aca54a2ba7c7a3ef7c5d7410738021bdcf742ec&mix_id=" + songID;
             Uri playuri = new Uri(uri);
             WebClient web = new WebClient();
             web.DownloadStringCompleted += new DownloadStringCompletedEventHandler(playEightTrackSong_Completed);
+
+            PlayEightTrackPlaySongCallback = callback;
             web.DownloadStringAsync(playuri);
         }
 
@@ -98,7 +109,7 @@ namespace PhoneApp1.Model
             (EightTrackPlaySong)dataContractJsonSerializer.ReadObject(stream);
 
             var result = myResults;
-
+            PlayEightTrackPlaySongCallback(myResults, null);
         }
     }
 }
